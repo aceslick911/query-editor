f4325ccc9268fbe468e38477b208dbf842eccb80
Angelo Perera <angeloperera@gmail.com>
2020-04-10 14:40:04 +1000

    Instance management

--- src/query-editor.js
+++ src/query-editor.js
@@ -3,18 +3,33 @@ import ReactDOM from "react-dom";
 
 import "./styles.less";
 
-const QueryEditor = ({ liked }) => {
-  const [state, setState] = useState({ liked });
+const QueryInstance = () => {
+  const QueryEditor = ({ liked }) => {
+    const [state, setState] = useState({ liked });
 
-  return (
-    <div class="query-editor">
-      <button onClick={() => setState({ liked: !state.liked })}>
-        {state.liked ? "You liked this" : "Like"}
-      </button>
-    </div>
-  );
+    return (
+      <div className="query-editor">
+        <button onClick={() => setState({ liked: !state.liked })}>
+          {state.liked ? "You liked this" : "Like"}
+        </button>
+      </div>
+    );
+  };
+
+  return {
+    component: QueryEditor,
+  };
 };
-
 export const create = ({ element, liked }) => {
-  ReactDOM.render(<QueryEditor liked={liked} />, element);
+  const instance = QueryInstance();
+  const QueryEditor = instance.component;
+  ReactDOM.render(<QueryEditor liked={liked}></QueryEditor>, element);
+  return {
+    on: ({ click }) => {
+      // Implement click handler
+    },
+    getState: () => {
+      // Implement return of state
+    },
+  };
 };
