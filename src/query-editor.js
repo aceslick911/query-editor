d615457d4c3a4eddf54a69263e242370e270f2f0
Angelo Perera <angeloperera@gmail.com>
2020-04-10 13:37:04 +1000

    React hooks working

--- src/query-editor.js
+++ src/query-editor.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const LikeButton = () => {
-  // const [state, setState] = useState({ liked: false });
+  const [state, setState] = useState({ liked: false });
 
   return (
-    // <button onClick={() => setState({ liked: true })}>
-    //   {state.liked ? "You liked this" : "Like"}
-    // </button>
-    <button>Hello</button>
+    <button onClick={() => setState({ liked: true })}>
+      {state.liked ? "You liked this" : "Like"}
+    </button>
   );
 };
 
-export default LikeButton;
+document.addEventListener("DOMContentLoaded", function (event) {
+  const rootElement = document.getElementById("class-editor");
+  ReactDOM.render(<LikeButton />, rootElement);
+});
