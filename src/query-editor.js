cf57482e5cbf16282f63938733c2957f35268f13
Angelo Perera <angelo.perera@nintex.com>
2020-04-19 10:24:36 +1000

    Update query data

--- src/query-editor.js
+++ src/query-editor.js
@@ -21,6 +21,8 @@ const QueryInstance = ({ queryState }) => {
   let clickHandler = null; // eslint-disable-line no-unused-vars
   let updateHandler = null; // eslint-disable-line no-unused-vars
 
+  let readQueryHandler = null;// eslint-disable-line no-unused-vars
+
   // eslint-disable-next-line react/prop-types
   const QueryEditor = ({ queryState }) => {
     const [state, doSetState] = useState({ ...queryState }); // eslint-disable-line no-unused-vars
@@ -34,6 +36,29 @@ const QueryInstance = ({ queryState }) => {
     };
 
     stateUpdater = setState;
+
+    readQueryHandler =(queryData)=>{
+      const rows = queryData.results.rows;
+      
+      // Map for fast access
+      const colMap = {}
+      for(let col of activeState.queryConfig.columns){
+        col.data=[];
+        colMap[col.dataSourceId]=col;
+      }
+  
+      for(let rowIndex = 0;rowIndex<rows.length;rowIndex++){
+        let inRow = rows[rowIndex];      
+        for(let inData in inRow){        
+          colMap[inData].data.push(inData)
+        }
+      }
+  
+      setState({
+        ...activeState,
+      })
+        
+    }
 
     const reorderQuery = ({ startIndex, newIndex }) => {
       setState({
@@ -108,14 +133,20 @@ const QueryInstance = ({ queryState }) => {
           columns:[...activeState.queryConfig.columns]
         }
       })
+    
+    
     }
-
+   
     return (
       <div className="query-editor">
         <QueryWindow state={state} reorderQuery={reorderQuery} addToQuery={addToQuery} removeFromQuery={removeFromQuery}></QueryWindow>
       </div>
     );
   };
+
+ 
+  
+
   return {
     component: <QueryEditor queryState={queryState}></QueryEditor>,
     getState: () => activeState,
@@ -130,8 +161,15 @@ const QueryInstance = ({ queryState }) => {
     updateState: (newState) => {
       if (stateUpdater) {
         stateUpdater(newState);
-      }
+      } 
     },
+    readQueryData:(queryData)=>{
+      if(readQueryHandler){
+        readQueryHandler(queryData)
+      } else{
+        throw new Error("readQueryHandler not assigned")
+      }
+    }
   };
 };
 export const create = ({ element, state }) => {
