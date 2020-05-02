c1329fbf598bf01e27366a0b88214c4a2c1ac9b1
Angelo Perera <angelo.perera@nintex.com>
2020-05-02 14:32:44 +1000

    Implementing dropzone

--- src/app/quint.js
+++ src/app/quint.js
@@ -1,24 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import ReactDOM from "react-dom";
+
+import { useDropzone } from "react-dropzone";
+
+import { API } from "../helpers/qds-api";
 
 import "./styles.less";
 
 const QuintInstance = ({ quintState }) => {
-  const Quint = () => {
+  const Quint = ({ state }) => {
+    const onDrop = useCallback(API.upload.uploadFiles, []);
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+      onDrop,
+    });
     return (
-      <div className="quint-main">
+      <div className="quint-main" {...getRootProps()}>
         <div className="flashCard">
           <div>
             <h1>Quint</h1>
             <div></div>
           </div>
         </div>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
       </div>
     );
   };
 
   return {
-    component: Quint(),
+    component: Quint({ state: quintState }),
   };
 };
 
--- src/helpers/qds-api.js
+++ src/helpers/qds-api.js
@@ -1,122 +1,182 @@
-
-
-const objectToQueryString = (obj) => Object.keys(obj).map(key => key + '=' + obj[key]).join('&');
-
+const objectToQueryString = (obj) =>
+  Object.keys(obj)
+    .map((key) => key + "=" + obj[key])
+    .join("&");
 
 export const API = {
-    query: {
-        // https://localhost:5001/api/Query/?query=
-        newRequest: (columns) => new Promise((resolve, reject) => {
-            fetch("/api/Query?" + objectToQueryString(
-                {
-                    query: columns
-                }
-            ), {
-                method: 'GET',
-                headers: new Headers()
-            })
-                .then((resp) => {
-                    if (resp.status >= 200 && resp.status < 300) {
-                        resp.json().then(queryId => {
-                            resolve(queryId)
-                        })
-                    } else {
-                        reject(resp)
-                    }
-                })
-        }),
-        // https://localhost:5001/api/Query/get?id=b3bddd09-d81a-4841-9bd9-bea458639053
-        getQuery: (queryId) => new Promise((resolve, reject) => {
-            console.log("Querying " + queryId);
-            fetch("/api/Query/get?" + objectToQueryString({
-                id: queryId
-            }), {
-                method: 'GET',
-                headers: new Headers()
-            })
-                .then((resp) => {
-                    if (resp.status >= 200 && resp.status < 300) {
-                        resp.json().then(query => {
-                            resolve(query)
-                        })
-                    } else {
-                        reject(resp)
-                    }
-                })
-        }),
-        // https://localhost:5001/api/Query/get?id=b3bddd09-d81a-4841-9bd9-bea458639053
-        getQueryResult: (queryId, progressUpdate, queryStartTime = Number(new Date())) => new Promise((resolve, reject) => {
-            console.log("Querying " + queryId);
-            API.query.getQuery(queryId).then((query) => {
-                const timeTaken = (Number(new Date()) - queryStartTime) / 1000;
-                console.log("Query time taken - " + timeTaken);
-                if (!query.plan.completed
-                    //||
-                    //Need to fix this as rows may be less than 100
-                    //query.plan.readyRows < 100
-                ) {
-                    console.log("Plan not ready.. waiting 1 second..", query.plan.phases, query.plan.progress)
-                    if (progressUpdate != null) {
-                        progressUpdate(query.plan);
-                    }
-                    setTimeout(() => {
-                        resolve(
-                            API.query.getQueryResult(queryId, progressUpdate, queryStartTime)
-                        );
-                    }, 1000);
-                } else {
-                    console.log("Query ready", query)
-                    // progressUpdate({
-                    //     phases: ["Complete"],
-                    //     timeRemaining: 0,
-                    //     progress: [1],
-                    // });
-                    resolve(
-                        API.query.getResultsFromQuery(queryId, 0, 100, query.plan.readyRows)
-                    )
-                }
-            }).catch(reason => reject(reason))
-        }),
-        // https://localhost:5001/api/Resultset/836558a0-0259-48fd-b968-121eacbe4d25?page=0&pagesize=20
-        getResultsFromQuery: (queryId, page, pagesize, totalRows = 1000) => new Promise((resolve, reject) => {
-            console.log("Querying " + queryId);
-            fetch("/api/Resultset/" + queryId + "?" + objectToQueryString({
-                page,
-                pagesize,
-            }), {
-                method: 'GET',
-                headers: new Headers()
-            })
-                .then((resp) => {
-                    if (resp.status >= 200 && resp.status < 300) {
-                        resp.json().then(query => {
-                            resolve({
-                                ...query,
-                                totalRows
-                            })
-                        })
-                    } else {
-                        reject(resp)
-                    }
-                })
-        }),
-        // https://localhost:5001/api/Query/All
-        getAllQueries: () => new Promise((resolve, reject) => {
-            fetch("/api/Query/All", {
-                method: 'GET',
-                headers: new Headers()
-            })
-                .then((resp) => {
-                    if (resp.status >= 200 && resp.status < 300) {
-                        resp.json().then(queries => {
-                            resolve(queries)
-                        })
-                    } else {
-                        reject(resp)
-                    }
-                })
-        }),
+  query: {
+    // https://localhost:5001/api/Query/?query=
+    newRequest: (columns) =>
+      new Promise((resolve, reject) => {
+        fetch(
+          "/api/Query?" +
+            objectToQueryString({
+              query: columns,
+            }),
+          {
+            method: "GET",
+            headers: new Headers(),
+          }
+        ).then((resp) => {
+          if (resp.status >= 200 && resp.status < 300) {
+            resp.json().then((queryId) => {
+              resolve(queryId);
+            });
+          } else {
+            reject(resp);
+          }
+        });
+      }),
+    // https://localhost:5001/api/Query/get?id=b3bddd09-d81a-4841-9bd9-bea458639053
+    getQuery: (queryId) =>
+      new Promise((resolve, reject) => {
+        console.log("Querying " + queryId);
+        fetch(
+          "/api/Query/get?" +
+            objectToQueryString({
+              id: queryId,
+            }),
+          {
+            method: "GET",
+            headers: new Headers(),
+          }
+        ).then((resp) => {
+          if (resp.status >= 200 && resp.status < 300) {
+            resp.json().then((query) => {
+              resolve(query);
+            });
+          } else {
+            reject(resp);
+          }
+        });
+      }),
+    // https://localhost:5001/api/Query/get?id=b3bddd09-d81a-4841-9bd9-bea458639053
+    getQueryResult: (
+      queryId,
+      progressUpdate,
+      queryStartTime = Number(new Date())
+    ) =>
+      new Promise((resolve, reject) => {
+        console.log("Querying " + queryId);
+        API.query
+          .getQuery(queryId)
+          .then((query) => {
+            const timeTaken = (Number(new Date()) - queryStartTime) / 1000;
+            console.log("Query time taken - " + timeTaken);
+            if (
+              !query.plan.completed
+              //||
+              //Need to fix this as rows may be less than 100
+              //query.plan.readyRows < 100
+            ) {
+              console.log(
+                "Plan not ready.. waiting 1 second..",
+                query.plan.phases,
+                query.plan.progress
+              );
+              if (progressUpdate != null) {
+                progressUpdate(query.plan);
+              }
+              setTimeout(() => {
+                resolve(
+                  API.query.getQueryResult(
+                    queryId,
+                    progressUpdate,
+                    queryStartTime
+                  )
+                );
+              }, 1000);
+            } else {
+              console.log("Query ready", query);
+              // progressUpdate({
+              //     phases: ["Complete"],
+              //     timeRemaining: 0,
+              //     progress: [1],
+              // });
+              resolve(
+                API.query.getResultsFromQuery(
+                  queryId,
+                  0,
+                  100,
+                  query.plan.readyRows
+                )
+              );
+            }
+          })
+          .catch((reason) => reject(reason));
+      }),
+    // https://localhost:5001/api/Resultset/836558a0-0259-48fd-b968-121eacbe4d25?page=0&pagesize=20
+    getResultsFromQuery: (queryId, page, pagesize, totalRows = 1000) =>
+      new Promise((resolve, reject) => {
+        console.log("Querying " + queryId);
+        fetch(
+          "/api/Resultset/" +
+            queryId +
+            "?" +
+            objectToQueryString({
+              page,
+              pagesize,
+            }),
+          {
+            method: "GET",
+            headers: new Headers(),
+          }
+        ).then((resp) => {
+          if (resp.status >= 200 && resp.status < 300) {
+            resp.json().then((query) => {
+              resolve({
+                ...query,
+                totalRows,
+              });
+            });
+          } else {
+            reject(resp);
+          }
+        });
+      }),
 
+    // https://localhost:5001/api/Query/All
+    getAllQueries: () =>
+      new Promise((resolve, reject) => {
+        fetch("/api/Query/All", {
+          method: "GET",
+          headers: new Headers(),
+        }).then((resp) => {
+          if (resp.status >= 200 && resp.status < 300) {
+            resp.json().then((queries) => {
+              resolve(queries);
+            });
+          } else {
+            reject(resp);
+          }
+        });
+      }),
+  },
+  upload: {
+    uploadFile: (file) => {
+      return new Promise((resolve, reject) => {
+        let url = "/Mvc/Upload/";
+        let formData = new FormData();
 
-    }
-}
+        formData.append("file", file);
+
+        return fetch(url, {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => {
+            resolve(true);
+            console.log("File uploaded", file, response);
+          })
+          .catch((reason) => {
+            reject(reason);
+          });
+      });
+    },
+
+    uploadFiles: (files) => {
+      console.log("uploading files", files);
+      files.forEach((file) => API.uploadFile(file));
+    },
+  },
+};
